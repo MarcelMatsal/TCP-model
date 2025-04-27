861faa60aa77bf89a55dcbb304f5863d47325096
Tyler Gurth <tyler_gurth@brown.edu>
2025-04-27 15:47:05 -0400

    z3 looks like z3

--- tahoe.py
+++ tahoe.py
@@ -31,13 +31,17 @@ class Tahoe(object):
         self.ssthresh = [Int(f'ssthresh_{t}') for t in range(T)]
         self.rtt = [Int(f'rtt_{t}') for t in range(T)]
         self.acks = [Int(f'ack_{t}') for t in range(T)]
+
+        for t in range(len(self.acks)):
+            self.s.add(self.acks[t] == int(acks[t]))  # Connect input list to model
     
     def solve_congestion(self):
         # We define an initial state
         self.s.add(self.cwnd[0] == 1)
-        self.s.add(self.ssthresh[0] == 16)
+        self.s.add(self.ssthresh[0] == 64)
         self.s.add(self.rtt[0] == 0)
 
+       
         # We loop through time and define the constraints of the Tahoe congestion control algorithm.
         for t in range(1, len(self.rtt)):
             # RTT should scale linearly
@@ -47,6 +51,28 @@ class Tahoe(object):
             self.s.add(self.ssthresh[t] > 0)
 
             # Based on ACKs, we have different constraints to follow:
+            cur_ack = self.acks[t - 1]
+            # First we handle the normal ACK
+            self.s.add(
+                If(cur_ack == int(AckType.NORMAL_ACK),
+                   If(self.cwnd[t - 1] < self.ssthresh[t - 1],
+                      And(self.cwnd[t] == self.cwnd[t-1] * 2, 
+                          self.ssthresh[t] == self.ssthresh[t-1]),
+                      And(self.cwnd[t] == self.cwnd[t-1] + 1,
+                          self.ssthresh[t] == self.ssthresh[t-1])),
+                    True)
+            )
+
+            # Then we handle the timeout ACK
+            self.s.add(
+                If(Or(cur_ack == int(AckType.TIMEOUT_ACK), cur_ack == int(AckType.DUPLICATE_ACK)),
+                    And(
+                        self.ssthresh[t] == self.cwnd[t-1] / 2,
+                        self.cwnd[t] == 1
+                    ),
+                    True)
+                )
+        
         
         # We see if with the given input, we can maintain a correct Tahoe
         if self.s.check() == sat:
@@ -58,14 +84,43 @@ class Tahoe(object):
        pass
 
 def produce_graph(tahoe_model, ans):
-    """Produce a graph of the congestion control algorithm"""
+    # """Produce a graph of the congestion control algorithm"""
+    # plt.xlabel('RTT (Round Trip Time)')
+    # plt.ylabel('Congestion Window Size')
+    # plt.title('Tahoe Congestion Control Algorithm')
+
+    # # We make use of the model and answer to plot the graph.
+
+    # cwnd_vals = [ans.evaluate(tahoe_model.cwnd[t]).as_long() for t in range(len(tahoe_model.cwnd))]
+    # rtt_vals = [ans.evaluate(tahoe_model.rtt[t]).as_long() for t in range(len(tahoe_model.rtt))]
+
+    # plt.plot(rtt_vals, cwnd_vals, marker='o')
+
+    # plt.show()
+    """Produce a graph of the congestion control algorithm with values of cwnd, ack, and ssthresh"""
     plt.xlabel('RTT (Round Trip Time)')
     plt.ylabel('Congestion Window Size')
     plt.title('Tahoe Congestion Control Algorithm')
 
     # We make use of the model and answer to plot the graph.
+    cwnd_vals = [ans.evaluate(tahoe_model.cwnd[t]).as_long() for t in range(len(tahoe_model.cwnd))]
+    rtt_vals = [ans.evaluate(tahoe_model.rtt[t]).as_long() for t in range(len(tahoe_model.rtt))]
+    ssthresh_vals = [ans.evaluate(tahoe_model.ssthresh[t]).as_long() for t in range(len(tahoe_model.ssthresh))]
+    ack_vals = [ans.evaluate(tahoe_model.acks[t]).as_long() for t in range(len(tahoe_model.acks))]
 
+    # Plot congestion window values.
+    plt.plot(rtt_vals, cwnd_vals, marker='o', label='cwnd')
 
+    # Annotate each point with its cwnd, ack, and ssthresh values.
+    for t in range(len(rtt_vals)):
+        plt.annotate(
+            f'{cwnd_vals[t]} (ack: {ack_vals[t]}, ssthresh: {ssthresh_vals[t]})',
+            (rtt_vals[t], cwnd_vals[t]),
+            textcoords="offset points",
+            xytext=(0, 5),
+            ha='center',
+            fontsize=8
+        )
 
     plt.show()
 
@@ -73,18 +128,18 @@ if __name__ == "__main__":
     acks = [
         AckType.NORMAL_ACK,
         AckType.NORMAL_ACK,
+        AckType.NORMAL_ACK,
+        AckType.NORMAL_ACK,
+        AckType.NORMAL_ACK,
+        AckType.NORMAL_ACK,
+        AckType.NORMAL_ACK,
         AckType.TIMEOUT_ACK,
         AckType.NORMAL_ACK,
-        AckType.DUPLICATE_ACK,
+        AckType.NORMAL_ACK,
+        AckType.NORMAL_ACK,
         AckType.NORMAL_ACK,
         AckType.NORMAL_ACK,
         AckType.TIMEOUT_ACK,
-        AckType.NORMAL_ACK,
-        AckType.NORMAL_ACK,
-        AckType.NORMAL_ACK,
-        AckType.NORMAL_ACK,
-        AckType.NORMAL_ACK,
-        AckType.DUPLICATE_ACK,
         AckType.NORMAL_ACK,
     ]
 
